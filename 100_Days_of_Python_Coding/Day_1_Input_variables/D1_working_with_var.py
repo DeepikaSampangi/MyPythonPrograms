1af2e3c4b027b7f3979055f81bbec6f83a64796c
deepikasampangi <deepikasampangi@gmail.com>
2021-12-28 19:50:39 +0530

    using_formatted_string_literal

--- 100_Days_of_Python_Coding/Day_1_Input_variables/D1_working_with_var.py
+++ 100_Days_of_Python_Coding/Day_1_Input_variables/D1_working_with_var.py
@@ -2,4 +2,5 @@ print("Name Generator Program")
 city: str = input("Enter the City you grew up in : ")
 pet_name: str = input("Enter the name of your pet : ")
 print("Your band name can be "+city+" "+pet_name)
+# Printing using Formatted String Literal
 print(f"Your band name can be {city} {pet_name}")
