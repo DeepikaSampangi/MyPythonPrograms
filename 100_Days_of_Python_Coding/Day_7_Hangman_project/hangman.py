888ad13e764aba70b46ce25974593b9b5917a29c
deepikasampangi <deepikasampangi@gmail.com>
2022-01-03 15:34:23 +0530

    hangman_game_code

--- 100_Days_of_Python_Coding/Day_7_Hangman_project/hangman.py
+++ 100_Days_of_Python_Coding/Day_7_Hangman_project/hangman.py
@@ -6,26 +6,32 @@ chosen_word = random.choice(word_list)
 
 answer = ["_" for _ in range(len(chosen_word))]
 print(logo)
-print(" ".join(answer))
 
 lives = 6
 print(stages[lives])
+print(" ".join(answer))
 
 game_over = False
+guessed_list = []
 
 while not game_over:
     guess = input("Enter the letter guessed:").lower()
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guess:
-            answer[i] = guess
+    if guess in guessed_list:
+        print(f"You have already guessed : {guess} ")
+    else:
+        guessed_list.append(guess)
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                answer[i] = guess
 
-    if "_" not in answer:
-        print(f"You won")
-        game_over = True
-    if guess not in chosen_word:
-        lives -= 1
-    if lives == 0:
-        game_over = True
+        if "_" not in answer:
+            print(f"You won")
+            game_over = True
+        if guess not in chosen_word:
+            lives -= 1
+            print(f"{guess} not in the Word, you lose a life")
+        if lives == 0:
+            game_over = True
     print(stages[lives])
     print(" ".join(answer))
 
