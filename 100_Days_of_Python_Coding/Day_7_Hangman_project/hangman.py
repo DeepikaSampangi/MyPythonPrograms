abb49968a154749945ad059f348b6cde87f24369
deepikasampangi <deepikasampangi@gmail.com>
2022-01-03 14:30:13 +0530

    fill_the_blanks

--- 100_Days_of_Python_Coding/Day_7_Hangman_project/hangman.py
+++ 100_Days_of_Python_Coding/Day_7_Hangman_project/hangman.py
@@ -1,24 +1,16 @@
-#Step 1 
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
 guess = input("Enter the letter guessed:").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-flag = False
+answer = ["_" for _ in range(len(chosen_word))]
+print(" ".join(answer))
 
-for letter in chosen_word:
-  if letter == guess:
-    flag = True
-    break
+for i in range(len(chosen_word)):
+    if chosen_word[i] == guess:
+        answer[i] = guess
 
-if flag:
-  print("Letter in the word")
-else:
-  print("Letter not in the word")
-
-answer = [_ for _ in range(len(chosen_word))]
+print(" ".join(answer))
