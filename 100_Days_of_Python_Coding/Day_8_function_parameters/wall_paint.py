8158889d363f07cb95f14f747c11c5d803c01957
deepikasampangi <deepikasampangi@gmail.com>
2022-01-04 15:23:56 +0530

    wall_paint_cans

---
+++ 100_Days_of_Python_Coding/Day_8_function_parameters/wall_paint.py
@@ -0,0 +1,12 @@
+import math
+
+def paint_calc(height: int, width: int, cover: int):
+    no_of_cans = (height * width) / cover
+    return math.ceil(no_of_cans)
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+can_count = paint_calc(height=test_h, width=test_w, cover=coverage)
+print(f"You'll need {can_count} cans of paint.")
