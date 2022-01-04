235fda46699616c16f85f7ca041953a314eddf88
deepikasampangi <deepikasampangi@gmail.com>
2022-01-04 15:15:17 +0530

    funtion_with_2_params

--- 100_Days_of_Python_Coding/Day_8_function_parameters/funtion_inputs.py
+++ 100_Days_of_Python_Coding/Day_8_function_parameters/funtion_inputs.py
@@ -13,3 +13,12 @@ def greet_with_name(name: str):
 
 greet_with_name(name="Universe")
 greet_with_name(name="milkyway")
+
+
+def greet_with_name_loc(name: str, location: str):
+    print(f"Hello {name}")
+    print("How is it going?")
+    print(f"What is it like at {location}")
+
+
+greet_with_name_loc(name="universe", location="milkyway")
