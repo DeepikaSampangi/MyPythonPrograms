ae53c7414dd99fd86fceeee62afe1443de3b91c7
deepikasampangi <deepikasampangi@gmail.com>
2022-01-04 15:33:15 +0530

    prime_number_checker

--- 100_Days_of_Python_Coding/Day_8_function_parameters/prime_number_check.py
+++ 100_Days_of_Python_Coding/Day_8_function_parameters/prime_number_check.py
@@ -1,6 +1,6 @@
 def is_prime(num: int):
-    count = 0
-    for i in range(1, num + 1):
+    count = 1
+    for i in range(2, num + 1):
         if num % i == 0:
             count += 1
             if count > 2:
