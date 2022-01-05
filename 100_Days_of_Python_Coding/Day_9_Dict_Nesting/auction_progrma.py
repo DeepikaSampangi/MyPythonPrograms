4d0e3aab69316111be74e55e85e4fce3aa92babf
deepikasampangi <deepikasampangi@gmail.com>
2022-01-05 15:42:09 +0530

    auction_program

--- 100_Days_of_Python_Coding/Day_9_Dict_Nesting/auction_progrma.py
+++ 100_Days_of_Python_Coding/Day_9_Dict_Nesting/auction_progrma.py
@@ -10,11 +10,12 @@ while True:
     user_resp = input("Do you want to add another bid?(yes/no): ")
     if user_resp != "yes":
         break
+
 max_bid = 0
 max_bidder = ""
-for key in digital_auction:
-    if digital_auction[key] > max_bid:
-        max_bid = digital_auction[key]
-        max_bidder = key
+for bidder in digital_auction:
+    if digital_auction[bidder] > max_bid:
+        max_bid = digital_auction[bidder]
+        max_bidder = bidder
 
 print(f"The heightest bidder is {max_bidder} with amount of {max_bid}")
