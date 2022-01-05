17351f5719623a28821247671317a3d932069f51
deepikasampangi <deepikasampangi@gmail.com>
2022-01-05 15:10:09 +0530

    dictionary_operations

--- 100_Days_of_Python_Coding/Day_9_Dict_Nesting/dictionary_examples.py
+++ 100_Days_of_Python_Coding/Day_9_Dict_Nesting/dictionary_examples.py
@@ -2,4 +2,20 @@ programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
 }
+
+# Retrieving Items
 print(programming_dictionary["Bug"])
+
+# Adding new item
+
+programming_dictionary["Loop"] = "The action of doing anything iteratively"
+print(programming_dictionary)
+
+# Create an empty dict.
+empyt_dict = {}
+
+# Edit an item
+programming_dictionary["Bug"] = "Error"
+print(programming_dictionary)
+
+#Loop through a dictionary.
