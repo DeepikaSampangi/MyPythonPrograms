d97a6df9510eb3a76f975cbd5c941e49f8cc16b0
deepikasampangi <deepikasampangi@gmail.com>
2022-01-06 17:37:26 +0530

    calculator

--- 100_Days_of_Python_Coding/Day_10_Functions_with_Output/calculator.py
+++ 100_Days_of_Python_Coding/Day_10_Functions_with_Output/calculator.py
@@ -19,21 +19,28 @@ def divide(n1: int, n2: int) -> float:
     return n1 / n2
 
 
-cal_ops = {"+": add, "-": subtract, "*": multiply, "/": divide}
+def calculator():
+    cal_ops = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
-num1 = int(input("What's the first number?: "))
-while True:
-    print("Select the operation to be performed")
-    for key in cal_ops:
-        print(key)
-    ops = input("Operation to be performed: ")
-    cal_func = cal_ops[ops]
-    next_num = int(input("What's the next number?: "))
+    num1 = int(input("What's the first number?: "))
+    while True:
+        print("Select the operation to be performed")
+        for key in cal_ops:
+            print(key)
+        ops = input("Operation to be performed: ")
+        cal_func = cal_ops[ops]
+        next_num = int(input("What's the next number?: "))
 
-    ans = cal_func(num1, next_num)
-    print(f"{num1} {ops} {next_num} = {ans}")
-    resp = input("Type 'y' to continue calculating with the prev, type 'n' to exit: ")
-    if resp != "y":
-        break
-    else:
-        num1 = ans
+        ans = cal_func(num1, next_num)
+        print(f"{num1} {ops} {next_num} = {ans}")
+        resp = input(
+            "Type 'y' to continue calculating with the prev, type 'n' to exit: "
+        )
+        if resp != "y":
+            break
+        else:
+            num1 = ans
+    calculator()
+
+
+calculator()
