0419436dfdfb3fefb267fde3b74de75779d8b2b0
deepikasampangi <deepikasampangi@gmail.com>
2022-01-09 18:36:22 +0530

    duebugging_handling

--- 100_Days_of_Python_Coding/Day_13_debugging/debugging_examples.py
+++ 100_Days_of_Python_Coding/Day_13_debugging/debugging_examples.py
@@ -7,11 +7,14 @@
 #       print("You got it")
 # my_function()
 
+# Solution : range(1,21)
+
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 # dice_num = randint(1, 6)
 # print(dice_imgs[dice_num])
+# Solution : randint(0,5)
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
