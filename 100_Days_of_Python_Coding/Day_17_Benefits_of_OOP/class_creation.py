e9e0f44d442e89c489e94599d056887d45b1bf34
deepikasampangi <deepikasampangi@gmail.com>
2022-01-13 12:26:01 +0530

    variable_accessing_ways

--- 100_Days_of_Python_Coding/Day_17_Benefits_of_OOP/class_creation.py
+++ 100_Days_of_Python_Coding/Day_17_Benefits_of_OOP/class_creation.py
@@ -1,16 +1,51 @@
+## Class Creation
+class User:
+    pass
+
+
+user_1 = User()
+
+
+## Class creation with init
 class User:
     def __init__(self, seats):
         # initialize the attributes
-        # self.seats = seats
-        print("Init")
-
-    def access(self):
+        self.seats = seats
         print(self.seats)
 
 
 user_1 = User(seats=10)
-user_1.access()
-# user_1.id = "001"
-# user_1.username = "Deepika"
 
-# print(user_1.username)
+## Class Creation with methods accessing init variables
+class User:
+    def __init__(self, seats):
+        # initialize the attributes
+        print("init")
+
+    def access(self):
+        print(self.seats)  # AttributeError 'User' object has no attribute 'seats'
+        # print(seats) Error no such variable defined in method
+
+
+user_1 = User(seats=10)
+# user_1.access()  ## Thorws an AttributeError
+
+## Class method tries to access var from another method
+class User:
+    def __init__(self, seats):
+        # initialize the attributes
+        self.seats = seats
+
+    def access(self):
+        print(self.seats)
+        self.new_var = 10
+        another_var = 4
+
+    def access_new_var(self):
+        self.access()
+        print(self.new_var)
+        # print(self.another_var) Throws Attribute Error'User' object has no attribute 'another_var'
+
+
+user_1 = User(seats=10)
+user_1.access_new_var()
