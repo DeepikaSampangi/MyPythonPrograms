cee05b80b0bc22edbac4975f94363d1cdb10eea7
deepikasampangi <deepikasampangi@gmail.com>
2022-01-13 12:13:32 +0530

    initializing_attributes_init

--- 100_Days_of_Python_Coding/Day_17_Benefits_of_OOP/class_creation.py
+++ 100_Days_of_Python_Coding/Day_17_Benefits_of_OOP/class_creation.py
@@ -1,11 +1,16 @@
 class User:
-    def __init__(self):
+    def __init__(self, seats):
         # initialize the attributes
-        pass
+        # self.seats = seats
+        print("Init")
+
+    def access(self):
+        print(self.seats)
 
 
-user_1 = User()
-user_1.id = "001"
-user_1.username = "Deepika"
+user_1 = User(seats=10)
+user_1.access()
+# user_1.id = "001"
+# user_1.username = "Deepika"
 
-print(user_1.username)
+# print(user_1.username)
