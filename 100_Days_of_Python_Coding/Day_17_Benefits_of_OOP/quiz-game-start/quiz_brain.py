b69feca36e7dbeb1aa154b4b441a8201bf5243f6
deepikasampangi <deepikasampangi@gmail.com>
2022-01-13 15:06:36 +0530

    quiz_game_logic

--- 100_Days_of_Python_Coding/Day_17_Benefits_of_OOP/quiz-game-start/quiz_brain.py
+++ 100_Days_of_Python_Coding/Day_17_Benefits_of_OOP/quiz-game-start/quiz_brain.py
@@ -2,11 +2,24 @@ class QuizBrain:
     def __init__(self, q_list) -> None:
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q{self.question_number}] {current_question.text} (True/False):")
+        user_ans = input(
+            f"Q{self.question_number}] {current_question.text} (True/False):"
+        )
+        self.check_answer(user_ans=user_ans, correct_ans=current_question.answer)
 
     def still_has_questions(self) -> bool:
         return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_ans, correct_ans):
+        if user_ans.lower() == correct_ans.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer is {correct_ans}")
+        print(f"Your current score is : {self.score}/{self.question_number}")
