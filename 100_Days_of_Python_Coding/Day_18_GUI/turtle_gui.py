66b594787acf8f38e20219ae79536ffc4302345b
deepikasampangi <deepikasampangi@gmail.com>
2022-01-14 10:37:51 +0530

    random_walk

--- 100_Days_of_Python_Coding/Day_18_GUI/turtle_gui.py
+++ 100_Days_of_Python_Coding/Day_18_GUI/turtle_gui.py
@@ -48,12 +48,11 @@ def draw_all_shapes():
 
 # draw_all_shapes()
 
-directions = [0, 90, 180, 270]
-tim.pensize(15)
-tim.speed("fastest")
-
 
 def random_walk():
+    directions = [0, 90, 180, 270]
+    tim.pensize(15)
+    tim.speed("fastest")
     for _ in range(200):
         tim.color(random.choice(colours))
         tim.forward(30)
