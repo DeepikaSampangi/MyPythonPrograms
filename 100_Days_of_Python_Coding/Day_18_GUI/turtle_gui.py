5ed3948edbc12e259139a7acf8a777e10bbbe467
deepikasampangi <deepikasampangi@gmail.com>
2022-01-14 09:41:19 +0530

    draw_square

--- 100_Days_of_Python_Coding/Day_18_GUI/turtle_gui.py
+++ 100_Days_of_Python_Coding/Day_18_GUI/turtle_gui.py
@@ -3,6 +3,14 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 
 timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+
+def draw_square():
+    for _ in range(4):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(90)
+
+draw_square()
 
 screen = Screen()
 screen.exitonclick()
