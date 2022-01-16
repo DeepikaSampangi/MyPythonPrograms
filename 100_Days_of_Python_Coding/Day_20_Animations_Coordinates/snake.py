89c79ed175d1f83a9597ce2825bbde368301ad84
deepikasampangi <deepikasampangi@gmail.com>
2022-01-16 19:39:54 +0530

    snake_movement_control

--- 100_Days_of_Python_Coding/Day_20_Animations_Coordinates/snake.py
+++ 100_Days_of_Python_Coding/Day_20_Animations_Coordinates/snake.py
@@ -11,6 +11,7 @@ class Snake:
     def __init__(self) -> None:
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in STARTING_POSITIONS:
@@ -25,4 +26,16 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DIST)
+        self.head.forward(MOVE_DIST)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def right(self):
+        self.head.setheading(0)
+
+    def left(self):
+        self.head.setheading(180)
