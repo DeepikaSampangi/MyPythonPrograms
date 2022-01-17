bb884fe424a83c011502a1d9c11502d2784ddba9
deepikasampangi <deepikasampangi@gmail.com>
2022-01-17 12:07:52 +0530

    food_refresh

--- 100_Days_of_Python_Coding/Day_21_Snake_Game_2/snake_game_main.py
+++ 100_Days_of_Python_Coding/Day_21_Snake_Game_2/snake_game_main.py
@@ -1,6 +1,7 @@
-from turtle import Screen, Turtle, position
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,9 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+
 game_is_on = True
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,6 +25,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.2)
     snake.move_snake()
+
+    # Detect Collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
