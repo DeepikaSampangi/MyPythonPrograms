77ce390e286ea542b8c39bb5452fff8c47e49d34
deepikasampangi <deepikasampangi@gmail.com>
2022-01-19 12:07:24 +0530

    initialize_cars

--- 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/car_manager.py
+++ 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/car_manager.py
@@ -1,11 +1,24 @@
 from turtle import Turtle
+import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
+STARTING_MOVE_DISTANCE = 10
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self) -> None:
-        super().__init__()
-        
+        self.cars = []
+
+    def initialize_cars(self):
+        for i in range(random.randint(1, 5)):
+            self.cars.append(Turtle(shape="square"))
+            self.cars[-1].shapesize(stretch_len=2, stretch_wid=1)
+            self.cars[-1].color(random.choice(COLORS))
+            self.cars[-1].penup()
+            self.cars[-1].goto(310, random.randint(-280, 280))
+            self.cars[-1].setheading(180)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(STARTING_MOVE_DISTANCE)
--- 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/turtlecrossing_main.py
+++ 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/turtlecrossing_main.py
@@ -10,6 +10,9 @@ screen.tracer(0)
 player = Player()
 
 scoreboard = Scoreboard()
+
+cars = CarManager()
+
 screen.listen()
 screen.onkey(player.up, "Up")
 screen.onkey(player.down, "Down")
@@ -17,12 +20,14 @@ screen.onkey(player.down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.5)
     screen.update()
     # Detect Reach to the end
     if player.ycor() > 275:
         scoreboard.level += 1
         player.reset()
         scoreboard.updated_score()
+    cars.initialize_cars()
+    cars.move_cars()
 
 screen.exitonclick()
