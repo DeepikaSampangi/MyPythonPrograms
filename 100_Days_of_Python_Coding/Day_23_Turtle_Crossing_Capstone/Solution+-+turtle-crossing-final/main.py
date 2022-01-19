d1dd8329fb21b703c556628e592015f3f61e5acb
deepikasampangi <deepikasampangi@gmail.com>
2022-01-19 14:37:51 +0530

    turtle_crossing_logic

--- 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/Solution+-+turtle-crossing-final/main.py
+++ 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/Solution+-+turtle-crossing-final/main.py
@@ -20,16 +20,16 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-    car_manager.initialize_cars()
+    car_manager.create_car()
     car_manager.move_cars()
 
-    # Detect collision with car
-    for car in car_manager.cars:
+    #Detect collision with car
+    for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
             scoreboard.game_over()
 
-    # Detect successful crossing
+    #Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
--- 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/Solution+-+turtle-crossing-final/scoreboard.py
+++ 100_Days_of_Python_Coding/Day_23_Turtle_Crossing_Capstone/Solution+-+turtle-crossing-final/scoreboard.py
@@ -1,23 +1,21 @@
 from turtle import Turtle
 
-
 FONT = ("Courier", 24, "normal")
-ALIGNMENT = "left"
 
 
 class Scoreboard(Turtle):
-    def __init__(self) -> None:
+
+    def __init__(self):
         super().__init__()
         self.level = 1
-        self.color("black")
-        self.penup()
-        self.goto(-275, 275)
-        self.updated_score()
         self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.update_scoreboard()
 
-    def updated_score(self):
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
 
     def increase_level(self):
         self.level += 1
