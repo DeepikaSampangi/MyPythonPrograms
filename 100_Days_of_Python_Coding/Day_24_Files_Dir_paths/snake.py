bd4ded0fb2824969bbee2d4f8c449ddafe8cfe4b
deepikasampangi <deepikasampangi@gmail.com>
2022-01-20 15:48:54 +0530

    snake_game_highscore

--- 100_Days_of_Python_Coding/Day_24_Files_Dir_paths/snake.py
+++ 100_Days_of_Python_Coding/Day_24_Files_Dir_paths/snake.py
@@ -1,5 +1,4 @@
 from turtle import Turtle
-
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -9,6 +8,7 @@ RIGHT = 0
 
 
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_snake()
@@ -24,13 +24,6 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
-
-    def reset(self):
-        for seg in self.segments:
-            seg.goto(1000, 1000)
-        self.segments.clear()
-        self.create_snake()
-        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
