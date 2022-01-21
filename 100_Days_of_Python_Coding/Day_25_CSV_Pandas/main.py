d4469bf1299dce89fe8d393da299cdb456503b30
deepikasampangi <deepikasampangi@gmail.com>
2022-01-21 11:59:34 +0530

    using_csv_package

--- 100_Days_of_Python_Coding/Day_25_CSV_Pandas/main.py
+++ 100_Days_of_Python_Coding/Day_25_CSV_Pandas/main.py
@@ -5,3 +5,9 @@ file_path = os.path.join(curr_dir, "weather_data.csv")
 # with open(file_path) as file:
 #     content = file.readlines()
 #     print(content)
+
+import csv
+
+with open(file_path) as file:
+    data = csv.reader(file)
+    print(data)
