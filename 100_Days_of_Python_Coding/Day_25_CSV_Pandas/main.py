7f5b450b355ba88884e9f116588a676c233ce247
deepikasampangi <deepikasampangi@gmail.com>
2022-01-21 11:53:10 +0530

    traditional_way_of_accessing_files

---
+++ 100_Days_of_Python_Coding/Day_25_CSV_Pandas/main.py
@@ -0,0 +1,7 @@
+import os
+
+curr_dir = os.path.dirname(__file__)
+file_path = os.path.join(curr_dir, "weather_data.csv")
+# with open(file_path) as file:
+#     content = file.readlines()
+#     print(content)
