d8e2339c1e0c7c832008fbf646c53ff0fc7dc3d5
deepikasampangi <deepikasampangi@gmail.com>
2022-01-21 15:18:38 +0530

    squirrel_dataset_pandas

---
+++ 100_Days_of_Python_Coding/Day_25_CSV_Pandas/squirrel_main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+full_sq_data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+grey_len = len(full_sq_data[full_sq_data["Primary Fur Color"] == "Gray"])
+red_len = len(full_sq_data[full_sq_data["Primary Fur Color"] == "Cinnamon"])
+black_len = len(full_sq_data[full_sq_data["Primary Fur Color"] == "Black"])
+
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_len, red_len, black_len]
+}
+
+print(data_dict)
+
+len_df = pd.DataFrame(data_dict)
+len_df.to_csv("squirrle_count.csv")
