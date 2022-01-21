37ca5e5c6a40881711ccfeb20c22d3e76456cd69
deepikasampangi <deepikasampangi@gmail.com>
2022-01-21 12:07:21 +0530

    extracting_temp_values

--- 100_Days_of_Python_Coding/Day_25_CSV_Pandas/main.py
+++ 100_Days_of_Python_Coding/Day_25_CSV_Pandas/main.py
@@ -11,3 +11,10 @@ import csv
 with open(file_path) as file:
     data = csv.reader(file)
     print(data)
+    temp = []
+    for row in data:
+        print(row)
+        if row[1] != "temp":
+            temp.append(int(row[1]))
+
+    print(temp)
