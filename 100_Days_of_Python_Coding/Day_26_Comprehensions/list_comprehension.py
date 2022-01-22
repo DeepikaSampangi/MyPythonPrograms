a3405c8806dfa38d8a7125680b3b6ffa2cc8f1b8
deepikasampangi <deepikasampangi@gmail.com>
2022-01-22 12:16:49 +0530

    conditional_comprehensions

--- 100_Days_of_Python_Coding/Day_26_Comprehensions/list_comprehension.py
+++ 100_Days_of_Python_Coding/Day_26_Comprehensions/list_comprehension.py
@@ -9,3 +9,15 @@ name = "Ananya"
 new_name_list = [letter for letter in name]
 print(new_name_list)
 print(list(name))
+
+doubled_list = [2 * n for n in range(1, 5)]
+print(doubled_list)
+
+
+names = ["Damon", "Stefan", "Caroline", "Elena", "Bonnie", "Katherine"]
+
+short_names = [name for name in names if len(name) < 6]
+print(short_names)
+
+long_names = [name.upper() for name in names if len(names) > 5]
+print(long_names)
