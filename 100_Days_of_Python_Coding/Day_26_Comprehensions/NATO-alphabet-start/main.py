f085c7151718b705721f7f0adc929185aac72b37
deepikasampangi <deepikasampangi@gmail.com>
2022-01-22 13:03:06 +0530

    comprehensions

--- 100_Days_of_Python_Coding/Day_26_Comprehensions/NATO-alphabet-start/main.py
+++ 100_Days_of_Python_Coding/Day_26_Comprehensions/NATO-alphabet-start/main.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 curr_dir = os.path.dirname(__file__)
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
-nato_df = pd.read_csv(curr_dir+"/nato_phonetic_alphabet.csv")
+nato_df = pd.read_csv(curr_dir + "/nato_phonetic_alphabet.csv")
 
-nato_dict = {row["letter"]:row["code"] for index, row in nato_df.iterrows()}
+nato_dict = {row["letter"]: row["code"] for index, row in nato_df.iterrows()}
 print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_input = input("Enter a word: ").upper()
 nato_output = [nato_dict[letter] for letter in user_input]
