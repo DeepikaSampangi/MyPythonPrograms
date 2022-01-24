b9b06e2f52041d48d14ebdfe5aaa869bb527ff84
deepikasampangi <deepikasampangi@gmail.com>
2022-01-24 18:49:33 +0530

    text_bok_button_entry_logic

--- 100_Days_of_Python_Coding/Day_27_Tkinter_GUI/tkinter_main.py
+++ 100_Days_of_Python_Coding/Day_27_Tkinter_GUI/tkinter_main.py
@@ -1,14 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
 
-my_label = tkinter.Label(text="I am Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am Label", font=("Arial", 24, "bold"))
 
 my_label.pack()
 
+# Button
+def button_click():
+    input_value = input.get()
+    my_label.config(text=input_value)
 
+
+button = Button(text="Submit", command=button_click)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
 window.mainloop()
