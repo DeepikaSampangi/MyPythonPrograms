8226cb5ad8adfee5b2e2d769ca31556af01268fe
deepikasampangi <deepikasampangi@gmail.com>
2022-01-24 18:41:08 +0530

    using_kwargs_in_class

--- 100_Days_of_Python_Coding/Day_27_Tkinter_GUI/args_examples.py
+++ 100_Days_of_Python_Coding/Day_27_Tkinter_GUI/args_examples.py
@@ -17,3 +17,15 @@ def calculate(n, **kwargs):
 
 
 calculate(2, add=2, mul=4)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
