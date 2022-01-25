4c06c58710c9238dd3ebd32393f6a7022ca69946
deepikasampangi <deepikasampangi@gmail.com>
2022-01-25 12:18:54 +0530

    time_format_dynamic_typing

--- 100_Days_of_Python_Coding/Day_28_Tkinter_Dynamic_typing_Pomodoro_GUI/pomodoro/main.py
+++ 100_Days_of_Python_Coding/Day_28_Tkinter_Dynamic_typing_Pomodoro_GUI/pomodoro/main.py
@@ -19,7 +19,7 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    count_down(1 * 60)
 
 
 def reset_timer():
@@ -30,6 +30,10 @@ def reset_timer():
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = "0" + str(count_sec)
+    if count_min < 10:
+        count_min = "0" + str(count_min)
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
@@ -47,7 +51,7 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=curr_dir + "/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(
-    100, 130, text="00.00", fill="white", font=(FONT_NAME, 35, "bold")
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
 )
 canvas.grid(column=1, row=1)
 
