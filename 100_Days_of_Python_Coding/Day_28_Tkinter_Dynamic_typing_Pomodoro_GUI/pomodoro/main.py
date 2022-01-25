dda44556219936f62cbda4605f7f0e43ee6893ae
deepikasampangi <deepikasampangi@gmail.com>
2022-01-25 12:19:38 +0530

    time_format

--- 100_Days_of_Python_Coding/Day_28_Tkinter_Dynamic_typing_Pomodoro_GUI/pomodoro/main.py
+++ 100_Days_of_Python_Coding/Day_28_Tkinter_Dynamic_typing_Pomodoro_GUI/pomodoro/main.py
@@ -19,7 +19,7 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(1 * 60)
+    count_down(5 * 60)
 
 
 def reset_timer():
@@ -31,9 +31,9 @@ def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
-        count_sec = "0" + str(count_sec)
+        count_sec = f"0{count_sec}"
     if count_min < 10:
-        count_min = "0" + str(count_min)
+        count_min = f"0{count_min}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
