544a692b791c0ee6b57b61d1048658d7dae2bb05
deepikasampangi <deepikasampangi@gmail.com>
2022-01-25 12:29:15 +0530

    pomodoro_logic

--- 100_Days_of_Python_Coding/Day_28_Tkinter_Dynamic_typing_Pomodoro_GUI/pomodoro/main.py
+++ 100_Days_of_Python_Coding/Day_28_Tkinter_Dynamic_typing_Pomodoro_GUI/pomodoro/main.py
@@ -1,5 +1,6 @@
 from cgitb import text
 import os
+from pickle import GLOBAL
 from tkinter import *
 import time
 import math
@@ -14,16 +15,33 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+REPS = 0
 # ---------------------------- TIMER RESET ------------------------------- #
-
-# ---------------------------- TIMER MECHANISM ------------------------------- #
-def start_timer():
-    count_down(5 * 60)
 
 
 def reset_timer():
     pass
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    global REPS
+    REPS += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if REPS in range(1, 9, 2):
+        title_label.config(text="Work Time", fg=GREEN)
+        count_down(work_sec)
+    elif REPS in range(2, 8, 2):
+        title_label.config(text="Short Break", fg=PINK)
+        count_down(short_break_sec)
+    elif REPS == 8:
+        title_label.config(text="Long Break", fg=RED)
+        count_down(long_break_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -37,6 +55,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -44,8 +64,8 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
-timer_label.grid(column=1, row=0)
+title_label = Label(text="Timer", font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
+title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=curr_dir + "/tomato.png")
