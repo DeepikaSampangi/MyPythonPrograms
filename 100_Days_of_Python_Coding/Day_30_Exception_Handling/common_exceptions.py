3e7ca72e32a15a90765353f615a1d7bcff890442
deepikasampangi <deepikasampangi@gmail.com>
2022-01-27 14:04:15 +0530

    try_except_blocks

--- 100_Days_of_Python_Coding/Day_30_Exception_Handling/common_exceptions.py
+++ 100_Days_of_Python_Coding/Day_30_Exception_Handling/common_exceptions.py
@@ -14,3 +14,15 @@
 # TypeError
 # text = "abc"
 # print(text + 5)
+
+# try:
+#     #Something that might caue an exception
+#     file = open("a_file.txt")
+# except FileNotFoundError:
+#     # Do this if there was an exception
+#     file = open("a_file.txt", "w")
+#     print("There was an error")
+# else:
+#     # Do this if there were no exceptions
+# finally:
+#     # Do this no matter what happens
