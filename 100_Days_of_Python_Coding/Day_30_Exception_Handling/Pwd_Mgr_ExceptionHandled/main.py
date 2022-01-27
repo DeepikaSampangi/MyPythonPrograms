7c754fee2f384bf6c7aac9d8f136b9e5fdf19544
deepikasampangi <deepikasampangi@gmail.com>
2022-01-27 14:28:21 +0530

    password_mgr_enhanced

--- 100_Days_of_Python_Coding/Day_30_Exception_Handling/Pwd_Mgr_ExceptionHandled/main.py
+++ 100_Days_of_Python_Coding/Day_30_Exception_Handling/Pwd_Mgr_ExceptionHandled/main.py
@@ -1,16 +1,72 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-import pyperclip
+import os
+
+# import pyperclip
 import json
 
+curr_dir = os.path.dirname(__file__)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
+# Password Generator Project
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
@@ -21,7 +77,8 @@ def generate_password():
 
     password = "".join(password_list)
     password_entry.insert(0, password)
-    pyperclip.copy(password)
+    # pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -37,21 +94,23 @@ def save():
     }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+        messagebox.showinfo(
+            title="Oops", message="Please make sure you haven't left any fields empty."
+        )
     else:
         try:
-            with open("data.json", "r") as data_file:
-                #Reading old data
+            with open(curr_dir + "/data.json", "r") as data_file:
+                # Reading old data
                 data = json.load(data_file)
         except FileNotFoundError:
-            with open("data.json", "w") as data_file:
+            with open(curr_dir + "/data.json", "w") as data_file:
                 json.dump(new_data, data_file, indent=4)
         else:
-            #Updating old data with new data
+            # Updating old data with new data
             data.update(new_data)
 
-            with open("data.json", "w") as data_file:
-                #Saving updated data
+            with open(curr_dir + "/data.json", "w") as data_file:
+                # Saving updated data
                 json.dump(data, data_file, indent=4)
         finally:
             website_entry.delete(0, END)
@@ -62,7 +121,7 @@ def save():
 def find_password():
     website = website_entry.get()
     try:
-        with open("data.json") as data_file:
+        with open(curr_dir + "/data.json") as data_file:
             data = json.load(data_file)
     except FileNotFoundError:
         messagebox.showinfo(title="Error", message="No Data File Found.")
@@ -70,9 +129,13 @@ def find_password():
         if website in data:
             email = data[website]["email"]
             password = data[website]["password"]
-            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}"
+            )
         else:
-            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+            messagebox.showinfo(
+                title="Error", message=f"No details for {website} exists."
+            )
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -82,11 +145,11 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
+logo_img = PhotoImage(file=curr_dir + "/logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
@@ -94,13 +157,13 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#Entries
+# Entries
 website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.insert(0, "xyz@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
