9adf8090bda78928d4485cd7f33b3339b42a7ba3
deepikasampangi <deepikasampangi@gmail.com>
2022-01-27 11:13:41 +0530

    common_exceptions

---
+++ 100_Days_of_Python_Coding/Day_30_Exception_Handling/common_exceptions.py
@@ -0,0 +1,16 @@
+# FileNotFound
+# with open("a_text.txt") as file:
+# file.read()
+
+
+# KeyError
+# a_dict = {"key": "value"}
+# value = a_dict["non_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[4]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
