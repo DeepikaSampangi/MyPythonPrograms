99a11a11c1c80a22fec9c1e04521b4e74af747da
deepikasampangi <deepikasampangi@gmail.com>
2022-01-28 14:41:44 +0530

    fetching_words_and_displaying

--- 100_Days_of_Python_Coding/Day_31_Flash_Card_Capstone/flash-card-project-start/main.py
+++ 100_Days_of_Python_Coding/Day_31_Flash_Card_Capstone/flash-card-project-start/main.py
@@ -1,28 +1,47 @@
 import os
+import random
+import pandas as pd
 from tkinter import *
-from turtle import bgcolor, xcor, ycor
+from PIL import ImageTk, Image
 
 curr_dir = os.path.dirname(__file__)
 BACKGROUND_COLOR = "#B1DDC6"
+
+df = pd.read_csv(curr_dir + "/data/french_words.csv")
+to_learn = df.to_dict(orient="records")
+
+
+def random_french_word():
+    curr_card = random.choice(to_learn)
+    lang = "French"
+    canvas.itemconfig(card_title, text=lang)
+    canvas.itemconfig(card_word, text=curr_card[lang])
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file=curr_dir + "/images/card_front.png")
+card_front_img = ImageTk.PhotoImage(Image.open(curr_dir + "/images/card_front.png"))
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="Word", fill="black", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(
+    400, 150, text="", fill="black", font=("Ariel", 40, "italic")
+)
+card_word = canvas.create_text(
+    400, 263, text="", fill="black", font=("Ariel", 60, "bold")
+)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-wrong_img = PhotoImage(file=curr_dir + "/images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_img = ImageTk.PhotoImage(Image.open(curr_dir + "/images/wrong.png"))
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=random_french_word)
 wrong_button.grid(row=1, column=0)
 
-right_img = PhotoImage(file=curr_dir + "/images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_img = ImageTk.PhotoImage(Image.open(curr_dir + "/images/right.png"))
+right_button = Button(image=right_img, highlightthickness=0, command=random_french_word)
 right_button.grid(row=1, column=1)
+
+random_french_word()
 
 window.mainloop()
