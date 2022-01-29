8923991362d17fd8a72fb3fc1867c26ab2e5d8f4
deepikasampangi <deepikasampangi@gmail.com>
2022-01-29 12:11:50 +0530

    random_quote_on_monday

--- 100_Days_of_Python_Coding/Day_32_SMTP/datetime_example.py
+++ 100_Days_of_Python_Coding/Day_32_SMTP/datetime_example.py
@@ -1,5 +1,8 @@
 import datetime as dt
+import os
+import random
 
+curr_dir = os.path.dirname(__file__)
 now = dt.datetime.now()
 now
 print(now.year)
@@ -8,3 +11,8 @@ print(now.day)
 
 date_of_birth = dt.datetime(year=1996, month=9, day=26)
 print(date_of_birth)
+
+if now.weekday == 0:
+    with open(curr_dir + "/quotes.txt") as file:
+        quotes = file.readlines()
+        print(random.choice(quotes))
