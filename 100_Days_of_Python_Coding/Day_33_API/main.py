00692c5684c343bbfe29a24ed353b07c2dda6390
deepikasampangi <deepikasampangi@gmail.com>
2022-01-30 15:08:18 +0530

    sunrise_sunset

--- 100_Days_of_Python_Coding/Day_33_API/main.py
+++ 100_Days_of_Python_Coding/Day_33_API/main.py
@@ -5,15 +5,32 @@
 # 5xx: I screwed Up
 
 import requests
+from datetime import  datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+#
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# latitude = data["iss_position"]["latitude"]
+# longitude = data["iss_position"]["longitude"]
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
 
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-
-latitude = data["iss_position"]["latitude"]
-longitude = data["iss_position"]["longitude"]
-iss_position = (longitude, latitude)
-
-print(iss_position)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunset)
+time_now = datetime.now()
+print(time_now)
