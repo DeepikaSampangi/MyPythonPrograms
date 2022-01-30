dc04364a8dde98474b8330a75b1b902f276f7985
deepikasampangi <deepikasampangi@gmail.com>
2022-01-30 14:52:01 +0530

    request_module_raise_for_status

--- 100_Days_of_Python_Coding/Day_33_API/main.py
+++ 100_Days_of_Python_Coding/Day_33_API/main.py
@@ -1,12 +1,14 @@
-import requests
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
-print(response.status_code)
-print(response.json())
-
 # 1xx: Hold On
 # 2xx: Here you go
 # 3xx: Go Away
 # 4xx: You Screwed Up
 # 5xx: I screwed Up
+
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+response.raise_for_status()
+
+data = response.json()
+print(data)
