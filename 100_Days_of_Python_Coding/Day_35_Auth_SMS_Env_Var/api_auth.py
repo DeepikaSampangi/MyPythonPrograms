660ce01668453e62d6ddbd55386c442f691eaa62
deepikasampangi <deepikasampangi@gmail.com>
2022-02-01 19:07:46 +0530

    fetch_weather_data_hourly

--- 100_Days_of_Python_Coding/Day_35_Auth_SMS_Env_Var/api_auth.py
+++ 100_Days_of_Python_Coding/Day_35_Auth_SMS_Env_Var/api_auth.py
@@ -1,14 +1,20 @@
 import requests
 
-api_key = ""
+api_key = "34ee58694a578e7f8c18874e8f0f0994"
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
 weather_params = {
-    "lat": 51.507351,
-    "lon": -0.127758,
-    "appid": api_key
+    "lat": 51.759050,
+    "lon": 19.458600,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 response = requests.get(url=OWM_endpoint, params=weather_params)
 response.raise_for_status()
 print(response.status_code)
-print(response.json())
+weather_data = response.json()
+hourly_data = weather_data["hourly"][:12]
+for data in hourly_data:
+    for i in data["weather"]:
+        if i["id"] < 700:
+            print("Get an umbrella")
