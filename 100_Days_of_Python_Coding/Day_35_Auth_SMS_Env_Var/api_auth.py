09900b9ffc14a04656d850e3f528a53636fdc5e4
deepikasampangi <deepikasampangi@gmail.com>
2022-02-01 15:23:32 +0530

    using_api_key

--- 100_Days_of_Python_Coding/Day_35_Auth_SMS_Env_Var/api_auth.py
+++ 100_Days_of_Python_Coding/Day_35_Auth_SMS_Env_Var/api_auth.py
@@ -1,8 +1,14 @@
-import http
 import requests
 
 api_key = "34ee58694a578e7f8c18874e8f0f0994"
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
-response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={api_key}")
+weather_params = {
+    "lat": 51.507351,
+    "lon": -0.127758,
+    "appid": api_key
+}
+response = requests.get(url=OWM_endpoint, params=weather_params)
 response.raise_for_status()
+print(response.status_code)
 print(response.json())
