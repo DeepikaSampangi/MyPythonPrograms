d605364b4f41014410f8f8a1bdb9e01734424ad8
deepikasampangi <deepikasampangi@gmail.com>
2022-02-01 19:12:05 +0530

    fetch_weather_data_hourly

--- 100_Days_of_Python_Coding/Day_35_Auth_SMS_Env_Var/api_auth.py
+++ 100_Days_of_Python_Coding/Day_35_Auth_SMS_Env_Var/api_auth.py
@@ -13,8 +13,14 @@ response = requests.get(url=OWM_endpoint, params=weather_params)
 response.raise_for_status()
 print(response.status_code)
 weather_data = response.json()
-hourly_data = weather_data["hourly"][:12]
-for data in hourly_data:
-    for i in data["weather"]:
-        if i["id"] < 700:
-            print("Get an umbrella")
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+for hour_data in weather_slice:
+    for data in hour_data["weather"]:
+        condition_code = data["id"]
+        if condition_code < 700:
+            will_rain = True
+
+if will_rain:
+    print("Bring an Umbrella")
