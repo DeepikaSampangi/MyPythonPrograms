3090bc6b90b4d8f7fddfceab7c97a8e20841e3cc
deepikasampangi <deepikasampangi@gmail.com>
2022-02-02 17:59:27 +0530

    using_aplha_api_for_stock_info

--- 100_Days_of_Python_Coding/Day_36_Stock_Trading_News_App/main.py
+++ 100_Days_of_Python_Coding/Day_36_Stock_Trading_News_App/main.py
@@ -1,8 +1,20 @@
+import requests
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+api_key = ""
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+alpha_url = "https://www.alphavantage.co/query"
+alpha_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": api_key
+}
+
+alpha_resp = requests.get(url=alpha_url, params=alpha_params)
+print(alpha_resp.json())
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
