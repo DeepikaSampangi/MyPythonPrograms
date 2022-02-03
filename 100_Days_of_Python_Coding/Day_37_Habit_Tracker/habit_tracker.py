652c44e6e911115a5ea4a3d8af50405717c704be
deepikasampangi <deepikasampangi@gmail.com>
2022-02-03 17:26:37 +0530

    create_pixela_graph

--- 100_Days_of_Python_Coding/Day_37_Habit_Tracker/habit_tracker.py
+++ 100_Days_of_Python_Coding/Day_37_Habit_Tracker/habit_tracker.py
@@ -15,3 +15,18 @@ params = {
 # pixela_user_resp = requests.post(url=url, json=params)
 # print(pixela_user_resp.text)
 
+graph_endpoint = f"{pixela_url}/{USERNAME}/graphs"
+
+graph_config = {
+    "id":"graph1",
+    "name": " Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color":"ajisai"
+}
+
+auth_header = {
+    "X-USER-TOKEN": TOKEN
+}
+graph_resp = requests.post(url=graph_endpoint, json=graph_config, headers=auth_header)
+print(graph_resp.text)
