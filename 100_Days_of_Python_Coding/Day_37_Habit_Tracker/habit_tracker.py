5315a46afccac429c2d883813c1a57b4e9903b78
deepikasampangi <deepikasampangi@gmail.com>
2022-02-03 17:34:18 +0530

    create_a_pixel_entry

--- 100_Days_of_Python_Coding/Day_37_Habit_Tracker/habit_tracker.py
+++ 100_Days_of_Python_Coding/Day_37_Habit_Tracker/habit_tracker.py
@@ -1,7 +1,9 @@
 import requests
+from datetime import datetime as dt
 
 USERNAME = "dsam"
 TOKEN = "e1ghtch@rs"
+GRAPH_ID = "graph1"
 
 pixela_url = "https://pixe.la/v1/users"
 
@@ -18,7 +20,7 @@ params = {
 graph_endpoint = f"{pixela_url}/{USERNAME}/graphs"
 
 graph_config = {
-    "id":"graph1",
+    "id": GRAPH_ID,
     "name": " Cycling Graph",
     "unit": "Km",
     "type": "float",
@@ -28,5 +30,13 @@ graph_config = {
 auth_header = {
     "X-USER-TOKEN": TOKEN
 }
-graph_resp = requests.post(url=graph_endpoint, json=graph_config, headers=auth_header)
-print(graph_resp.text)
+# graph_resp = requests.post(url=graph_endpoint, json=graph_config, headers=auth_header)
+# print(graph_resp.text)
+
+pixel_endpoint = f"{pixela_url}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_config = {
+    "date": "20220202",
+    "quantity": "5.0"
+}
+pixel_resp = requests.post(url=pixel_endpoint, json=pixel_config, headers=auth_header)
+print(pixel_resp.text)
