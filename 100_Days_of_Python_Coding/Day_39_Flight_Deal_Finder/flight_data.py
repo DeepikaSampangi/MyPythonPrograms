eb98f8c7610d9cf27f2423a539d486548a5d4d2d
deepikasampangi <deepikasampangi@gmail.com>
2022-02-12 16:30:22 +0530

    flight_deal_finder

--- 100_Days_of_Python_Coding/Day_39_Flight_Deal_Finder/flight_data.py
+++ 100_Days_of_Python_Coding/Day_39_Flight_Deal_Finder/flight_data.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import os
+
+curr_dir = os.path.dirname(__file__)
+
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self):
+        self.flight_deals = pd.read_csv(curr_dir+"/flight_deal_prices.csv")
+
+    def get_flight_deals(self):
+        print(self.flight_deals.head())
+
+FlightData().get_flight_deals()
