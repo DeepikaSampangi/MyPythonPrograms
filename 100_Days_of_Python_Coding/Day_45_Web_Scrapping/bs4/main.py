7cd0566c224604e9281cd1b65b26fc15072b7417
deepikasampangi <deepikasampangi@gmail.com>
2022-02-21 21:14:40 +0530

    using_selectors

--- 100_Days_of_Python_Coding/Day_45_Web_Scrapping/bs4/main.py
+++ 100_Days_of_Python_Coding/Day_45_Web_Scrapping/bs4/main.py
@@ -11,3 +11,6 @@ all_a_tags = soup.find_all(name="a")
 for tag in all_a_tags:
     print(tag.getText())
     print(tag.get("href"))
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
