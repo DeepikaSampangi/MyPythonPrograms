60e86d2d274dca269b857a57c213abf7474d5326
deepikasampangi <deepikasampangi@gmail.com>
2022-02-24 19:01:35 +0530

    selenium_find_elements

--- 100_Days_of_Python_Coding/Day_48_Selenium_WebDriver/main.py
+++ 100_Days_of_Python_Coding/Day_48_Selenium_WebDriver/main.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 
 chrome_driver_path = "./chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+url="https://www.python.org/"
+driver.get(url)
 
-driver.get("https://www.amazon.in")
+headline = driver.find_element("dive-into-python")
+print(headline.text)
 
 driver.close()
