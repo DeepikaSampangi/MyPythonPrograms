fbface55e91221f1875ce8890576a2c1c5ed8257
deepikasampangi <deepikasampangi@gmail.com>
2022-03-07 19:06:13 +0530

    funtc_as_first_class_obj

---
+++ 100_Days_of_Python_Coding/Day_54_Intro_Flask/python_decorators.py
@@ -0,0 +1,25 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+# First-class Obj, can be passed around as arguments
+
+
+def calculate(cal_func, n1, n2):
+    return cal_func(n1, n2)
+
+
+result = calculate(multiply, 2, 3)
+print(result)
