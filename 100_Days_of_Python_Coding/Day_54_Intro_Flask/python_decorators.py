575f8116b74b73c7b34572863868dfbebbe6d973
deepikasampangi <deepikasampangi@gmail.com>
2022-03-07 19:14:09 +0530

    decorator_function

--- 100_Days_of_Python_Coding/Day_54_Intro_Flask/python_decorators.py
+++ 100_Days_of_Python_Coding/Day_54_Intro_Flask/python_decorators.py
@@ -1,37 +1,56 @@
-def add(n1, n2):
-    return n1 + n2
+# def add(n1, n2):
+#     return n1 + n2
+#
+#
+# def subtract(n1, n2):
+#     return n1 - n2
+#
+#
+# def multiply(n1, n2):
+#     return n1 * n2
+#
+#
+# def divide(n1, n2):
+#     return n1 / n2
+#
+#
+# # First-class Obj, can be passed around as arguments
+#
+#
+# def calculate(cal_func, n1, n2):
+#     return cal_func(n1, n2)
+#
+#
+# result = calculate(multiply, 2, 3)
+# print(result)
+#
+#
+# # Nested funtions
+# def outer_func():
+#     print("I'm Outer")
+#
+#     def nested_func():
+#         print("I'm Inner")
+#
+#     nested_func()
+#
+# outer_func()
+
+# Python Decorator
+import time
 
 
-def subtract(n1, n2):
-    return n1 - n2
+def delay_deco(function):
+    def wrapper_func():
+        time.sleep(2)
+        function()
+
+    return wrapper_func
 
 
-def multiply(n1, n2):
-    return n1 * n2
+@delay_deco
+def say_hello():
+    print("Hello")
 
 
-def divide(n1, n2):
-    return n1 / n2
-
-
-# First-class Obj, can be passed around as arguments
-
-
-def calculate(cal_func, n1, n2):
-    return cal_func(n1, n2)
-
-
-result = calculate(multiply, 2, 3)
-print(result)
-
-
-# Nested funtions
-def outer_func():
-    print("I'm Outer")
-
-    def nested_func():
-        print("I'm Inner")
-
-    nested_func()
-
-outer_func()
+say_hello()
