7cbc44c7559e4772c9681caa332402a3c982e5ed
deepikasampangi <deepikasampangi@gmail.com>
2022-03-10 14:29:35 +0530

    english_words

--- python_general_tricks/Wordle-Helper/word_generator.py
+++ python_general_tricks/Wordle-Helper/word_generator.py
@@ -1,4 +1,9 @@
-import ast
+from english_words import english_words_set
+from ast import literal_eval
+from itertools import product
+from string import ascii_lowercase
 
-word_len = input("Enter word Length:")
-contians_words = input("Enter comma separated letters contained in the word:")
+word_len = literal_eval(input("Enter word Length:"))
+contains_words = literal_eval(input("Enter comma separated letters contained in the word:"))
+n_letter_words = [''.join(x) for x in product(ascii_lowercase, repeat=word_len)]
+print(n_letter_words)
