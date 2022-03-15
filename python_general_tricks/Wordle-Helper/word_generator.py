eabbc065d6aafd55deb5e2894836ac7d60364baf
deepikasampangi <deepikasampangi@gmail.com>
2022-03-15 19:47:23 +0530

    word_generator

--- python_general_tricks/Wordle-Helper/word_generator.py
+++ python_general_tricks/Wordle-Helper/word_generator.py
@@ -1,7 +1,7 @@
-from english_words import english_words_set
-from ast import literal_eval
-from itertools import product
-from string import ascii_lowercase
+# from english_words import english_words_set
+# from ast import literal_eval
+# from itertools import product
+# from string import ascii_lowercase
 
 word_len = literal_eval(input("Enter word Length:"))
 contains_words = literal_eval(
