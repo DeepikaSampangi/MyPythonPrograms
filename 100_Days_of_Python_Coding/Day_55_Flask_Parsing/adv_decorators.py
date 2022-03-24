4cd29d99ca9060d7a874f1fba9790c5999fb70a3
deepikasampangi <deepikasampangi@gmail.com>
2022-03-24 19:10:12 +0530

    formatting

--- 100_Days_of_Python_Coding/Day_55_Flask_Parsing/adv_decorators.py
+++ 100_Days_of_Python_Coding/Day_55_Flask_Parsing/adv_decorators.py
@@ -3,15 +3,19 @@ class User:
         self.name = name
         self.is_logged_in = False
 
+
 def is_auth_deco(function):
     def wrapper(*args, **kwargs):
         if args[0].is_logged_in:
             function(args[0])
+
     return wrapper
+
 
 @is_auth_deco
 def create_blog_post(user):
     print(f"This is {user.name}'s new blog post")
+
 
 new_user = User("Deepika")
 new_user.is_logged_in = True
