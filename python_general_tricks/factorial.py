a13a5b6d2e48d496b23d0e2ca168c4709710f860
deepikasampangi <deepikasampangi@gmail.com>
2022-04-04 20:26:37 +0530

    formatting

--- python_general_tricks/factorial.py
+++ python_general_tricks/factorial.py
@@ -1,9 +1,8 @@
 def factorial(num):
     if num == 0:
         return 1
-    return num*factorial(num-1)
+    return num * factorial(num - 1)
 
 
 input_num = int(input("Enter a number"))
 print(f"Factorial of {input_num} is {factorial(input_num)}")
-
