dabe579d89692df9f1346c5c2f9661c96d8109da
deepikasampangi <deepikasampangi@gmail.com>
2022-04-07 12:30:12 +0530

    String replace

---
+++ python_general_tricks/str_logics.py
@@ -0,0 +1,8 @@
+# Logic to remove ith Char in a string
+
+input_str = "amazing"
+print(f"Old String {input_str}")
+new_str = input_str.replace("a", "")
+print(f"New String {new_str}")
+
+input_str = "amazing"
