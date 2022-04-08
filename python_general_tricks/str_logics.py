401bc79e479044254f4025868118e0b9f2eb8711
deepikasampangi <deepikasampangi@gmail.com>
2022-04-08 12:30:12 +0530

    String replace

--- python_general_tricks/str_logics.py
+++ python_general_tricks/str_logics.py
@@ -5,4 +5,8 @@ print(f"Old String {input_str}")
 new_str = input_str.replace("a", "")
 print(f"New String {new_str}")
 
+# removing all occurances
 input_str = "amazing"
+print(f"Old String {input_str}")
+new_str = input_str.replace("a", "", 2)
+print(f"New String {new_str}")
