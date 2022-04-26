c92c73759dba66c137ef0a2781edd6464689defb
deepikasampangi <deepikasampangi@gmail.com>
2022-04-26 12:19:20 +0530

    zip function

--- python_general_tricks/tricks.py
+++ python_general_tricks/tricks.py
@@ -5,3 +5,9 @@ print(input_list)
 
 #Generator Function
 print(sum(i for i in range(10)))
+
+#Using zip function
+year = (1996, 2004, 2010, 2022)
+month = ("sept", "march", "dec", "jan")
+day = (26, 10, 25, 1)
+print(list(zip(year, month, day)))
