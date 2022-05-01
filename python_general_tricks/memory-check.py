4c847159f7c61635effadfbabd25f9fabdb71b57
deepikasampangi <deepikasampangi@gmail.com>
2022-05-01 20:37:47 +0530

    formatting

--- python_general_tricks/memory-check.py
+++ python_general_tricks/memory-check.py
@@ -1,5 +1,6 @@
 import sys
-a, b, c, d = "abcde","xy", 2, 15.06
+
+a, b, c, d = "abcde", "xy", 2, 15.06
 print(sys.getsizeof(a))
 print(sys.getsizeof(b))
 print(sys.getsizeof(c))
--- python_general_tricks/tricks.py
+++ python_general_tricks/tricks.py
@@ -1,12 +1,12 @@
 # Reverse List
-input_list = ["e","c","n","a","d"]
+input_list = ["e", "c", "n", "a", "d"]
 input_list.reverse()
 print(input_list)
 
-#Generator Function
+# Generator Function
 print(sum(i for i in range(10)))
 
-#Using zip function
+# Using zip function
 year = (1996, 2004, 2010, 2022)
 month = ("sept", "march", "dec", "jan")
 day = (26, 10, 25, 1)
@@ -14,16 +14,16 @@ print(list(zip(year, month, day)))
 
 
 # Transpose of Matrix
-input_matrix = [[10,21],[3,20],[16,28]]
+input_matrix = [[10, 21], [3, 20], [16, 28]]
 print(list(zip(*input_matrix)))
 
-#Print n times
+# Print n times
 input_str = "Hello"
-print(input_str*4)
+print(input_str * 4)
 
-#Factors of a number
+# Factors of a number
 
 num = 32
-for i in range(1, num+1):
-    if num%i == 0:
+for i in range(1, num + 1):
+    if num % i == 0:
         print(i)
