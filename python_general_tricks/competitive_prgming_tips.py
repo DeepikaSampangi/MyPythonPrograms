dbd9ad3230f505e54faa567c5bb4ae8677aacf16
Deepika Sampangi <deepikasampangi@gmail.com>
2022-05-16 19:20:20 +0530

    zipping_dict

--- python_general_tricks/competitive_prgming_tips.py
+++ python_general_tricks/competitive_prgming_tips.py
@@ -64,3 +64,15 @@ import heapq
 grades = [110, 25, 38, 49, 20, 95, 33, 87, 80, 90]
 print(heapq.nlargest(3, grades))
 print(heapq.nsmallest(4, grades))
+
+#zip dict
+import heapq
+stocks = {'Goog'   : 520.54,
+          'FB'     : 76.45,
+          'YHOO'   : 39.28,
+          'AMZN'   : 306.21,
+          'APPL'   : 99.76}
+zip_1 = zip(stocks.values(), stocks.keys())
+print(sorted(zip_1))
+zip_2 = zip(stocks.keys(), stocks.values())
+print(sorted(zip_2))
