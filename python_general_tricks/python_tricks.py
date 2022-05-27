95754ad49af4bc76dfa3ce5de54a16ee607b2d2b
Deepika Sampangi <deepikasampangi@gmail.com>
2022-05-27 10:12:46 +0530

    format

--- python_general_tricks/python_tricks.py
+++ python_general_tricks/python_tricks.py
@@ -1,6 +1,7 @@
 ## printing a sorted list of the input array of space separated numbers
 in_text = input()
 arr = map(int, in_text.split())
+
 print(set(arr))
 print(list(arr))
 print(sorted(list(set(arr))))
